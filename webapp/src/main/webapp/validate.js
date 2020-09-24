0322f4f6571242ec7aa116682d7c3479fbca06e7
Fabimauri47 <44916470+Fabimauri47@users.noreply.github.com>
2020-09-24 17:36:38 -0500

    act

--- webapp/src/main/webapp/validate.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-  var x = document.forms["numero"]["identificador"].value;
-  if (isNaN(parseFloat(x))) {
-    alert("Dato no valido ingrese un número");
-    println("false");
-  }
-  println("True");
-}
